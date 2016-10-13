f8ef0cbb9b4709885207f1ebd720905623516fb1
Fabien <fabien.webdev@gmail.com>
2016-10-13 14:04:33 +0900

    Script to get random quote from the quotes json file

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,36 @@
 $( document ).ready(function() {
 
   // Get started!
-  
+
+
+  $.getJSON("/assets/js/quotes.json", function(json){
+
+    //Make a string of my data
+    var text = JSON.stringify(json);
+
+    // Make an object of the string
+    var obj = JSON.parse(text);
+
+    // Get the length of my quotes array
+    var objLength = obj.quotes.length;
+
+    var randomNum = Math.floor(Math.random() * objLength);
+
+    // Get the data from my quotes[randomNum] object
+
+    var id = parseInt(obj.quotes[randomNum].id) + 1;
+    var quote = obj.quotes[randomNum].quote;
+    var author = obj.quotes[randomNum].author;
+    var image = obj.quotes[randomNum].image;
+
+    var msg = 'id: ' + id ;
+    msg += '<br/> quote: ' + quote;
+    msg += '<br/> author: ' + author;
+    msg += '<br/> image: ' + image;
+
+
+    $('#quote').html(msg);
+
+  });
 
 });
